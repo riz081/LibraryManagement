9d00b2763c41299bb0bbfe2643fa56562a65cf14
Rizqika <rizqikaibnu@gmail.com>
2024-09-25 01:03:26 +0700

    change test files to pest

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -1,19 +1,7 @@
 <?php
 
-namespace Tests\Feature;
-
-// use Illuminate\Foundation\Testing\RefreshDatabase;
-use Tests\TestCase;
-
-class ExampleTest extends TestCase
-{
-    /**
-     * A basic test example.
-     */
-    public function test_the_application_returns_a_successful_response(): void
-    {
-        $response = $this->get('/');
-
-        $response->assertStatus(200);
-    }
-}
+// Pest style
+test('the application return a successfull response', function () {
+    $response = $this->get('/');
+    $response->assertStatus(200);
+}) ;
